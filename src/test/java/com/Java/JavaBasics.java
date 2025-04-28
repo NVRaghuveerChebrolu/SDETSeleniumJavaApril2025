621f23f6dba38e78bf63e63de520018be9483684
Naga Venkata Raghuveer Chebrolu <raghuchnv@gmail.com>
2025-04-28 14:37:19 +0200

    Java basics

--- src/test/java/com/Java/JavaBasics.java
+++ src/test/java/com/Java/JavaBasics.java
@@ -1,10 +1,15 @@
 package com.Java;
 
 public class JavaBasics {
-
+	int a =9;//instance variable : defined inside a class and outside a method
+	static int b =4;// static variable 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int c =2; //local variable : declared inside a method
+		JavaBasics obj = new JavaBasics();
+		System.out.println(obj.a);
+		System.out.println(b);
+		System.out.println(c);
 	}
 
 }
